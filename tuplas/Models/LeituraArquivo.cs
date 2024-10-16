b5da0a87293afa8c07eec2f4454f67c14349f3b6
Tiago Souza <tiago0214@gmail.com>
2024-10-16 12:05:46 -0300

    att

---
+++ tuplas/Models/LeituraArquivo.cs
@@ -0,0 +1,20 @@
+﻿namespace tuplas.Models
+{
+    public class LeituraArquivo
+    {
+        public (bool Successo, string[] Linhas, int QuantidadeDeLinhas) LerArquivo(string caminho)
+        {
+            try
+            {
+                string[] arquivo = File.ReadAllLines(caminho);
+                return (true, arquivo, arquivo.Length);
+            }
+            catch (Exception ex)
+            {
+                return (false, new string[0], 0);
+            }
+
+        }
+
+    }
+}
---
+++ tuplas/Program.cs
@@ -0,0 +1,22 @@
+﻿using tuplas.Models;
+
+LeituraArquivo arquivo = new LeituraArquivo();
+
+var loja = arquivo.LerArquivo("./arquivoTest.txt");
+
+Console.WriteLine(loja);
+
+// everthing is a different ways of writing a tuple: tuples is a good way when I don't want to use class.
+
+// var outroExemplo = Tuple.Create(1, "Tiago", "Souza");
+// Console.WriteLine(outroExemplo.Item1);
+
+// ValueTuple<int, string, string> outroExemplo = (1, "Tiago", "Souza");
+// Console.WriteLine(outroExemplo.Item1);
+
+// (int id, string nome, string sobreNome) tupla = (1, "Tiago", "Souza");
+// Console.WriteLine(tupla.id);
+
+
+// (int, string, string) tupla = (1, "Tiago", "Souza");
+// Console.WriteLine(tupla.Item1);
